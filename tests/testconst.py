4444c1b6927512fa98ad6b9a2844dd49aa13a2ff
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-24 21:06:25 +0200

    Add shorthands for consts in testconsts

--- tests/testconst.py
+++ tests/testconst.py
@@ -5,9 +5,17 @@ TEST_LEVEL_1 = 'level1'
 TEST_LEVEL_2 = 'level2'
 TEST_LEVEL_3 = 'level3'
 
+L1 = TEST_LEVEL_1
+L2 = TEST_LEVEL_2
+L3 = TEST_LEVEL_3
+
 TEST_LEVEL_1_FILE = 'level1_file'
 TEST_LEVEL_2_FILE = 'level2_file'
 TEST_LEVEL_3_FILE = 'level3_file'
+
+L1_FILE = TEST_LEVEL_1_FILE
+L2_FILE = TEST_LEVEL_2_FILE
+L3_FILE = TEST_LEVEL_3_FILE
 
 # Overwritten at setup
 TEST_DIR = Path("./tests/testdir").resolve().absolute()
