b65e6bf0aae1b53adf4f452354d55c91103c3dea
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-25 15:27:52 +0200

    Add custom TestCase baseclass
    Create a fs structure
    Creates a few layers
    Creates a few files

---
+++ tests/TestBasicLayerCase.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append("./src")
+
+import unittest
+import subprocess
+import os
+from testconst import TEST_DIR, TEST_LEVEL_1, TEST_LEVEL_2, TEST_LEVEL_3
+from unittest import TestCase
+
+from BasicLayerCase import BasicLayerCase
+from GlobalConsts import SET_CONFIG_FILE
+from Layer import Layer
+
+
+
+class TestBasicLayerCase(BasicLayerCase):
+	def setUp(self):
+		super().setUp()
+		super().sync()
+
+	def tearDown(self):
+		super().tearDown()
+
+	def test_Setup(self):
+		self.assertCountEqual(super().verify(), [])
+
